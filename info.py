3a817b61ab00f4d8851ccdb55654b9b82ca42d40
HARDIK1317 <88221344+HARDIK1317@users.noreply.github.com>
2025-03-03 16:28:07 +0530

    Update info.py

--- info.py
+++ info.py
@@ -22,7 +22,7 @@ ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ
 USERNAME = environ.get('USERNAME', "https://t.me/riteshmakwana") # ADMIN USERNAME
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002326105086'))
 MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/+I-1eBvfQjZ1lMmM1')
-CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002375948779').split()]
+CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002236628317').split()]
 #---------------------------------------------------------------
 #---------------------------------------------------------------
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://riteshmakwana23:riteshmakwana23@hardikmm.xgnlb.mongodb.net/?retryWrites=true&w=majority&appName=hardikmm")
